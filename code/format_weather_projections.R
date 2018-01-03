d0922794156c0bc8f2d844933f09846cedbacf44
Andrew Tredennick <atredenn@gmail.com>
2018-01-02 20:25:01 -0700

    finishes weather proj code; saves formatted projections

--- code/format_weather_projections.R
+++ code/format_weather_projections.R
@@ -25,12 +25,23 @@ library(stringr)
 
 
 ####
-####  READ IN DATA AND ADD ROW COLUMN INFORMATION ------------------------------
+####  READ IN DATA, ADD ROW COLUMN INFORMATION, SAVE ---------------------------
 ####
 col_names <- c("year",
                "month",
                "day",
-               as.character(as.data.frame(read.delim("../data/CMIP_YNP/bcca5/COLS_pr.txt"))[,1])
+               as.character(as.data.frame(read.table("../data/CMIP_YNP/bcca5/COLS_pr.txt"))[,1])
                )
-raw_precip_projs <- read_csv("../data/CMIP_YNP/bcca5/pr.csv", col_names = FALSE)
+
+read_csv("../data/CMIP_YNP/bcca5/pr.csv", col_names = col_names) %>%
+  gather(key = model, value = ppt, -year, -month, -day) %>%
+  separate(model, into = c("model_name", "rep", "scenario"), sep = "[.]") %>%
+  group_by(year, month, model_name, scenario) %>%
+  summarise(total_ppt_mm = sum(ppt)) %>%
+  dplyr::filter(month == 1) %>%
+  dplyr::select(model_name, scenario, year, month, total_ppt_mm) %>%
+  dplyr::arrange(model_name, scenario, year, month) %>%
+  saveRDS(file = "../data/cmip_ynp_formatted_projections.RDS")
+
+
 
